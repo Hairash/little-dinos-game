886c5e238d67b13feb7cde5b5bb30a1864890785
Ivan Grebenshchikov <ivan.grebenshchikov@cloudblue.com>
2023-02-05 14:51:18 +0100

    Add Units
    * Add images

--- src/components/GameGrid.vue
+++ src/components/GameGrid.vue
@@ -3,8 +3,8 @@
     <div class="cell_line" v-for="(line, y) in field" :key=y>
       <template v-for="(cellData, x) in line" :key=x>
         <GameCell
-          :width=100
-          :height=100
+          :width=cellWidth
+          :height=cellHeight
           :terrain=cellData.terrain
           :unit=cellData.unit
         />
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-// import Unit from '../game/engine'
+import Engine from '../game/engine'
 import GameCell from './GameCell.vue'
 
 export default {
@@ -28,26 +28,46 @@ export default {
     height: Number,
   },
   data() {
-    let field = [];
-    for (let x = 0; x < this.width; x++) {
-      const col = [];
-      for (let y = 0; y < this.height; y++) {
-        // const r = Math.random();
-        col.push({
-          terrain: 'wall',
-          // unit: new Unit(),
-        });
-      }
-      field.push(col);
-    }
-    console.log(field);
+    const field = this.generateField();
+    const cellWidth = 100;
+    const cellHeight = 100;
     return {
       field,
+      cellWidth,
+      cellHeight,
+      cssProps: {
+        cellHeight: `${cellHeight}px`,
+      },
+    }
+  },
+  methods: {
+    generateField() {
+      const field = [];
+      const terrainTypesArr = Array.from(Engine.TerrainTypes);
+      for (let y = 0; y < this.height; y++) {
+        const col = [];
+        for (let x = 0; x < this.width; x++) {
+          const r = Math.random();
+          const terrain = terrainTypesArr[Math.floor(r * terrainTypesArr.length)];
+          const cell = {
+            terrain: terrain,
+          }
+          if (r < 0.05) cell.unit = new Engine.Unit(0, 'dino1');
+          else if (r < 0.1) cell.unit = new Engine.Unit(1, 'dino2');
+          col.push(cell);
+        }
+        field.push(col);
+      }
+      console.log(field);
+      return field;
     }
   }
 }
 </script>
 
 <style scoped>
+.cell_line {
+  height: v-bind('cssProps.cellHeight');
+}
 
 </style>
