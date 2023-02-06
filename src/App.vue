c407583051351a515fb120c45c6c7d08bb503d14
Ivan Grebenshchikov <ivan.grebenshchikov@cloudblue.com>
2023-02-07 00:51:23 +0100

    Add "End turn" button
    * Only current player's units may move

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <GameGrid :width=8 :height=6 />
+  <GameGrid :width=8 :height=6 :currentPlayer="currentPlayer"/>
+  <EndTurnBtn @click="processEndTurn"/>
 </template>
 
 <script>
 import GameGrid from './components/GameGrid.vue'
+import EndTurnBtn from './components/EndTurnBtn.vue'
 
 export default {
   name: 'App',
   components: {
     GameGrid,
+    EndTurnBtn,
+  },
+  data() {
+    const playersNum = 2;
+    let currentPlayer = 0;
+    return {
+      playersNum,
+      currentPlayer,
+    }
+  },
+  methods: {
+    processEndTurn() {
+      this.currentPlayer += 1;
+      this.currentPlayer %= this.playersNum;
+      console.log(this.currentPlayer);
+    }
   },
 }
 </script>
---
+++ src/components/EndTurnBtn.vue
@@ -0,0 +1,15 @@
+<template>
+  <button type="button">End turn</button>
+</template>
+
+<script>
+export default {
+  name: "EndTurnBtn",
+}
+</script>
+
+<style scoped>
+button {
+  margin-top: 20px;
+}
+</style>
