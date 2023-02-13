c9765fa82f4c4de95766c45c51c2fd52ef7ce03c
Hairash <maslevian@yandex.ru>
2023-02-13 01:28:08 +0100

    + End turn by press Enter key or right mouse click + Change start conditions. One base from the beginning + Active units label + Undo 1 movement by middle mouse click

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <GameGrid :field="field" :currentPlayer="currentPlayer" @moveUnit="moveUnit"/>
-  <span class="curPlayerLabel">Current player:
-    <!-- TODO: Fix it. Make images for players (not units) -->
-    <img class="curPlayerImage" :src="`/images/dino${currentPlayer + 1}.png`">
-  </span>
-  <EndTurnBtn @click="processEndTurn"/>
+  <GameGrid :field="field" :currentPlayer="currentPlayer" @moveUnit="moveUnit" />
+  <div class="infoLabel">
+    <span class="curPlayerLabel">Current player:
+      <!-- TODO: Fix it. Make images for players (not units) -->
+      <img class="curPlayerImage" :src="`/images/dino${currentPlayer + 1}.png`">
+    </span>
+    <span class="curActiveUnitsLabel">Active units: {{ getCurrentActiveUnits() }}</span>
+    <EndTurnBtn @click="processEndTurn" />
+  </div>
 </template>
 
 <script>
@@ -21,19 +24,46 @@ export default {
   data() {
     const playersNum = 4;
     let currentPlayer = 0;
-    const width = 28;
+    const width = 26;
     const height = 16;
     let field = null;
+    let prevField = null;
     return {
       playersNum,
       currentPlayer,
       width,
       height,
       field,
+      prevField,
     }
   },
   created() {
     this.field = this.generateField();
+    window.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') this.processEndTurn();
+    });
+    window.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      this.processEndTurn();
+    });
+    window.addEventListener('mouseup', (e) => {
+      e.preventDefault();
+      if (e.button === 1 && this.prevField) {
+        // console.log('Revert');
+        // console.log(this.prevField);
+        // TODO: What a hell?!
+        for (let x = 0; x < this.width; x++) {
+          for (let y = 0; y < this.height; y++) {
+            const prevCell = this.prevField[x][y];
+            const cell = this.field[x][y];
+            cell.unit = prevCell.unit;
+            cell.building = prevCell.building;
+          }
+        }
+        // this.field = structuredClone(this.prevField);
+        // console.log(this.field);
+      }
+    });
   },
   methods: {
     generateField() {
@@ -65,16 +95,22 @@ export default {
           x = Math.floor(Math.random() * this.width);
           y = Math.floor(Math.random() * this.height);
         }
+        field[x][y].building = new Engine.Building(
+          player,
+          Engine.BuildingTypes.BASE,
+        );
         field[x][y].unit = new Engine.Unit(
           player,
           `dino${player + 1}`,
-            5,
+          Math.ceil(Math.random() * 10),
         );
       }
       // console.log(field);
       return field;
     },
     moveUnit(fromCoords, toCoords) {
+      this.prevField = structuredClone(this.field);
+      // console.log(this.prevField);
       const [x0, y0] = fromCoords;
       const [x1, y1] = toCoords;
       const unit = this.field[x0][y0].unit;
@@ -82,11 +118,11 @@ export default {
       delete(this.field[x0][y0].unit);
       this.field[x1][y1].unit = unit;
       if (this.field[x1][y1].building) this.field[x1][y1].building.player = unit.player;
-      console.log(this.field[x1][y1]);
+      // console.log(this.field[x1][y1]);
       this.killNeighbours(x1, y1, unit.player);
     },
     killNeighbours(x, y, player) {
-      console.log('Kill')
+      // console.log('Kill')
       const neighbours = this.getNeighbours(x, y);
       for (const neighbour of neighbours) {
         const [curX, curY] = neighbour;
@@ -126,7 +162,20 @@ export default {
           }
         }
       }
-    }
+      this.prevField = null;
+    },
+    getCurrentActiveUnits() {
+      let ctr = 0;
+      for (let x = 0; x < this.width; x++) {
+        for (let y = 0; y < this.height; y++) {
+          const unit = this.field[x][y].unit;
+          if (unit && unit.player === this.currentPlayer && !unit.hasMoved) {
+            ctr++;
+          }
+        }
+      }
+      return ctr;
+    },
   },
 }
 </script>
@@ -141,6 +190,10 @@ export default {
   margin-top: 60px;
 }
 
+div.infoLabel {
+  color: white;
+}
+
 span.curPlayerLabel {
   margin-right: 30px;
 }
@@ -149,5 +202,9 @@ img.curPlayerImage {
   width: 30px;
   height: 30px;
   vertical-align: bottom;
+}
+
+span.curActiveUnitsLabel {
+  margin-right: 30px;
 }
 </style>
