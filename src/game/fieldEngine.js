26c7699f7e4f61e369482f0922b4bcb76f2a4d3e
Hairash <maslevian@yandex.ru>
2024-06-27 17:11:43 +0200

    Refactoring

--- src/game/fieldEngine.js
+++ src/game/fieldEngine.js
@@ -1,14 +1,16 @@
 // Helpers related to field
-import {getNeighbours} from "@/game/helpers";
+import { createNewUnit, getNeighbours } from "@/game/helpers";
 import { SCORE_MOD } from "@/game/const";
 
 export class FieldEngine {
-  constructor(field, width, height, fogOfWarRadius, players) {
+  constructor(field, width, height, fogOfWarRadius, players, minSpeed, maxSpeed) {
     this.field = field;
     this.width = width;
     this.height = height;
     this.fogOfWarRadius = fogOfWarRadius;
     this.players = players;
+    this.minSpeed = minSpeed;
+    this.maxSpeed = maxSpeed;
   }
 
   getCurrentVisibilitySet(player) {
@@ -55,17 +57,58 @@ export class FieldEngine {
     );
   }
 
-  killNeighbours(field, x, y, curPlayer, countScore=true) {
+  moveUnit(x0, y0, x1, y1, unit) {
+    unit.hasMoved = true;
+    delete(this.field[x0][y0].unit);
+    this.field[x1][y1].unit = unit;
+  }
+
+  killNeighbours(x, y, curPlayer, countScore=true) {
     // console.log('Kill')
-    const neighbours = getNeighbours(field, this.width, this.height, x, y);
+    const neighbours = getNeighbours(this.field, this.width, this.height, x, y);
     for (const neighbour of neighbours) {
       const [curX, curY] = neighbour;
-      if (field[curX][curY].unit && field[curX][curY].unit.player !== curPlayer) {
+      if (this.field[curX][curY].unit && this.field[curX][curY].unit.player !== curPlayer) {
         this.players[curPlayer].killed++;
         if (countScore) this.changeScore(curPlayer, SCORE_MOD.kill);
-        this.players[field[curX][curY].unit.player].lost++;
-        delete(field[curX][curY].unit);
+        this.players[this.field[curX][curY].unit.player].lost++;
+        delete(this.field[curX][curY].unit);
       }
+    }
+  }
+
+  captureBuildingIfNeeded(x1, y1, player) {
+    if (this.field[x1][y1].building) this.field[x1][y1].building.player = player;
+  }
+
+  restoreAndProduceUnits(curPlayer) {
+    let buildingsNum = 0;
+    let unitsNum = 0;
+    let producedNum = 0;
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        if (this.field[x][y].unit) {
+          this.field[x][y].unit.hasMoved = false;
+          if (this.field[x][y].unit.player === curPlayer) {
+            unitsNum++;
+          }
+        }
+        if (this.field[x][y].building && this.field[x][y].building.player === curPlayer) {
+          buildingsNum++;
+          if (!this.field[x][y].unit) {
+            this.field[x][y].unit = createNewUnit(curPlayer, this.minSpeed, this.maxSpeed);
+            producedNum++;
+            if (this.killAtBirth) {
+              this.fieldEngine.killNeighbours(x, y, curPlayer, false);
+            }
+          }
+        }
+      }
+    }
+    return {
+      buildingsNum: buildingsNum,
+      unitsNum: unitsNum,
+      producedNum: producedNum,
     }
   }
 
