34b1de8cbcea461c199e2cff6c281936f033e2cc
Hairash <maslevian@yandex.ru>
2024-12-28 00:32:36 +0100

    Re-design info panel
    * Close #72

--- src/App.vue
+++ src/App.vue
@@ -61,8 +61,14 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=RocknRoll+One&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "RocknRoll One", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
